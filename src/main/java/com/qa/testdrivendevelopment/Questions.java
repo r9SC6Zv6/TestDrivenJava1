9ccf76c567d7b88e7849f60b77b1929fec0433cc
Ervin Musincki <emusincki@academytrainee.com>
2021-01-25 14:49:33 +0000

    Solutions to task uploaded

---
+++ src/main/java/com/qa/testdrivendevelopment/Questions.java
@@ -0,0 +1,277 @@
+package com.qa.testdrivendevelopment;
+
+import java.util.Arrays;
+
+public class Questions {
+
+	/**
+	 * EXAMPLE: THIS ONE HAS BEEN DONE FOR YOU <br>
+	 * 
+	 * Given a name, return "Hi" plus the name <br>
+	 * <br>
+	 * For Example:<br>
+	 * greetingExample("John") → "Hi John" <br>
+	 * greetingExample("Matt") → "Hi Matt" <br>
+	 * greetingExample("Angelica") → "Hi Angelica"
+	 * 
+	 */
+	public String greetingExample(String name) {
+		return "Hi " + name;
+	}
+
+	/**
+	 * Given a string, return a string where for every char in the original string,
+	 * there are three chars.<br>
+	 * <br>
+	 * 
+	 * For Example:<br>
+	 * multChar("The") → "TTThhheee"<br>
+	 * multChar("AAbb") → "AAAAAAbbbbbb"<br>
+	 * multChar("Hi-There") → "HHHiii---TTThhheeerrreee"
+	 */
+	public String multiChar(String input) {
+		String output = "";
+		String[] inputArray = input.split("");
+ 		for (String c : inputArray) {
+				output = output + c + c + c;
+		}
+		
+		return output;
+	}
+
+	/**
+	 * Return the string (backwards) that is between the first and last appearance
+	 * of "bread" in the given string, or return the empty string "" if there is not
+	 * 2 occurances of "bread". Ignore Case<br>
+	 * <br>
+	 * For Example: sandwichFilling("breadclivebread") → "evilc"<br>
+	 * sandwichFilling("xxbreadfridgebreadyy") → "egdirf"<br>
+	 * sandwichFilling("xxBreadfridgeBReAdyy") → "egdirf"<br>
+	 * sandwichFilling("xxbreadyy") → "" sandwichFilling("xxbreADyy") → ""<br>
+	 * <br>
+	 * HINT: "a" == "a" if false HINT: "a".equals("a") is true
+	 */
+
+	public String sandwichFilling(String sandwich) {
+		sandwich = sandwich.toLowerCase();
+		int firstBread = sandwich.indexOf("bread");
+		if (firstBread == -1)
+			return "";
+		String postFirstBread = sandwich.substring(firstBread + 5);
+		int secondBread = postFirstBread.indexOf("bread");
+		if (secondBread == -1)
+			return "";
+		String betweenBread = postFirstBread.substring(0, secondBread);
+		String[] reversedBetweenBread = betweenBread.split("");
+		String output = "";
+		for (int i = reversedBetweenBread.length; i > 0; i--) {
+			output = output + reversedBetweenBread[i-1];
+		}
+		return output;
+	}
+
+	/**
+	 * Given three ints, a b c, one of them is small, one is medium and one is
+	 * large. Return true if the three values are evenly spaced, so the difference
+	 * between small and medium is the same as the difference between medium and
+	 * large. Do not assume the ints will come to you in a reasonable order.<br>
+	 * <br>
+	 * For Example:<br>
+	 * evenlySpaced(2, 4, 6) → true<br>
+	 * evenlySpaced(4, 6, 2) → true<br>
+	 * evenlySpaced(4, 6, 3) → false<br>
+	 * evenlySpaced(4, 60, 9) → false
+	 */
+	public boolean evenlySpaced(int a, int b, int c) {
+		
+		int[] toOrder = {a, b, c};
+		Arrays.sort(toOrder);
+		
+		
+		if (toOrder[1] - toOrder[0] == toOrder[2] - toOrder[1])
+			return true;
+		return false;
+	}
+
+	/**
+	 * Given a string and an int n, return a string that removes n letters from
+	 * the'middle' of the string. The string length will be at least n. The input
+	 * string will have an odd number of letters. n will always be an odd
+	 * number.<br>
+	 * <br>
+	 * For Example:<br>
+	 * nMid("Hello", 3) → "Ho"<br>
+	 * nMid("Chocolate", 3) → "Choate"<br>
+	 * nMid("Chocolate", 1) → "Choclate"<br>
+	 */
+	public String nMid(String input, int n) {
+		
+		String leftInput = input.substring(0, (input.length()-1)/2 - (n-1)/2);
+		String rightInput = input.substring((input.length()+1)/2 + (n-1)/2);
+		return leftInput + rightInput;
+	}
+
+	/**
+	 * Given a string, return the boolean True if it ends in "java" and False if
+	 * does not end in "java". Ignore Case.<br>
+	 * <br>
+	 * For Example: <br>
+	 * endsJava("ilovejava") → true <br>
+	 * endsJava("iloveJaVa") → true <br>
+	 * endsJava("welovejava") → true <br>
+	 * endsJava("welovejavamoreeveryday") → false <br>
+	 * endsJava("javaiscool") → false <br>
+	 * endsJava("pythoniscool") → false <br>
+	 */
+	public boolean endsJava(String input) {
+		
+		return (input.toLowerCase().endsWith("java"));
+			
+	}
+
+	/**
+	 * Given a string, return the length of the largest "block" in the string. A
+	 * block is a run of adjacent chars that are the same, do not ignore case.<br>
+	 * <br>
+	 * For Example: superBlock("hooopplla") → 3 <br>
+	 * superBlock("abbCCCddDDDDeeEEE") → 4 <br>
+	 * superBlock("abbCCCCC")) → 5 <br>
+	 * superBlock("") → 0 <br>
+	 * <br>
+	 * HINT: "a" == "a" if false HINT: "a".equals("a") is true
+	 */
+	public int superBlock(String input) {
+		
+		String inputChar[] = input.split("");
+		
+		int currentCharCount = 0;
+		int largestCount = 0;
+		
+		for (int i = 1; i < inputChar.length; i++) {
+			if (inputChar[i].equals(inputChar[i-1]))
+				currentCharCount++;
+			else
+				currentCharCount = 0;
+			if (currentCharCount + 1 > largestCount)
+				largestCount = currentCharCount + 1;
+		}
+		
+		return largestCount;
+	}
+
+	/**
+	 * Given a string - return the number of times "am" appears in the String
+	 * ignoring case - BUT ONLY WHEN the word "am" appears without being followed or
+	 * preceded by other letters <br>
+	 * <br>
+	 * For Example: <br>
+	 * amISearch("Am I in Amsterdam") → 1 <br>
+	 * amISearch("I am in Amsterdam am I?") → 2 <br>
+	 * amISearch("I have been in Amsterdam") → 0 <br>
+	 * <br>
+	 * HINT: String.toLowerCase
+	 */
+	public int amISearch(String sentence) {
+		
+		sentence = sentence.toLowerCase();
+		
+		String[] sentenceArray = sentence.split(" ");
+		
+		int amCount = 0;
+		
+		for (String a : sentenceArray) {
+			if (a.equals("am"))
+				amCount++;
+		}
+		
+		return amCount;
+	}
+
+	/**
+	 * Given a number if this number is divisible by 3 return "fizz" if this number
+	 * is divisible by 5 return "buzz" if this number is divisible by both 3 and 5
+	 * return "fizzbuzz" if this number is not divisible by 3 or 5 return null<br>
+	 * <br>
+	 * For Example: <br>
+	 * fizzBuzz(3) → "fizz" <br>
+	 * fizzBuzz(10) → "buzz" <br>
+	 * fizzBuzz(15) → "fizzbuzz" <br>
+	 * fizzBuzz(8) → null
+	 */
+	public String fizzBuzz(int number) {
+		
+		String output = "";
+		
+		if (number % 3 != 0 && number % 5 != 0)
+			return null;
+		if (number % 3 == 0) 
+			output = "fizz";
+		if (number % 5 == 0)
+			output = output + "buzz";
+		
+		return output;
+	}
+
+	/**
+	 * Given a string, split the string into the individual numbers present then add
+	 * each digit of each number to get a final value for each number <br>
+	 * <br>
+	 * String example = "55 72 86"<br>
+	 * <br>
+	 * "55" will = the integer 10 <br>
+	 * "72" will = the integer 9 <br>
+	 * "86" will = the integer 14 <br>
+	 * <br>
+	 * You then need to return the highest vale <br>
+	 * <br>
+	 * For Example: <br>
+	 * largest("55 72 86") → 14 <br>
+	 * largest("15 72 80 164") → 11 <br>
+	 * largest("555 72 86 45 10") → 15 <br>
+	 * <br>
+	 * HINT: Integer.parseInt
+	 */
+
+	public int largest(String input) {
+		String[] inputArray = input.split(" ");
+		int currentInt = 0;
+		int maxSum = 0;
+		for (int i = 0; i < inputArray.length; i++) {
+			int currentSum = 0;
+			currentInt = Integer.parseInt(inputArray[i]);
+			while (currentInt > 0) {
+				currentSum = currentSum + (currentInt % 10);
+				currentInt /= 10;
+			}
+			if (currentSum > maxSum)
+				maxSum = currentSum;
+		}
+		return maxSum;
+	}
+
+	/**
+	 * Given a string, int and a char, return a boolean value if the 'nth'
+	 * (represented by the int provided) char of the String supplied is the same as
+	 * the char supplied. The int provided will NOT always be less than than the
+	 * length of the String. Ignore case and Whitespace. <br>
+	 * <br>
+	 * For Example: <br>
+	 * compares("The",2,'h') → true <br>
+	 * compares("AAbb",1,'b') → false <br>
+	 * compares("Hi-There",10,'e') → false <br>
+	 * <br>
+	 * HINT: String.charAt
+	 */
+	public boolean compares(String word, int index, char letter) {
+		
+		word = word.toLowerCase().replaceAll(" ", "");
+		if (index >= word.length())
+			return false;
+		
+		if (word.charAt(index-1) == letter) 
+			return true;
+		
+		
+		return false;
+	}
+}
